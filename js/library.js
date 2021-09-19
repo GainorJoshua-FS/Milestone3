4cdf216bb3214e10c7eedd6d934b57181cef78f6
GainorJoshua-FS <JQGainor@student.fullsail.edu>
2021-09-19 02:47:47 -0400

    Fixed some validation changes

---
+++ js/library.js
@@ -0,0 +1,125 @@
+window.addEventListener("load", function(){
+    console.log("page loaded");
+    //instantiate singleton
+    let library = Library.getInstance();
+});
+
+class Library{
+    constructor(){
+        let mylibrary = new MyLibrary();
+    }
+
+    static getInstance(){
+        if(!Library._instance){
+            Library._instance = new Library();
+            return Library._instance;
+        }
+        else{
+            throw "Singleton already created in librar!";
+        }
+    }
+}
+
+class MyLibrary{
+    constructor(){
+        let addGame = document.querySelector('.addBtn').addEventListener('click', e => this.AddModal(e));
+    }
+
+    AddModal(e){
+        e.preventDefault();
+        
+        const main = document.querySelector('main');
+
+        let modal = document.createElement('div');
+        modal.className = 'modalGame';
+        modal.setAttribute('id', 'modalWindow');
+        modal.innerHTML = '<div class="modal-content"><form class="container col-sm-10 col-md-8 col-lg-6 generateForm" id="newgame" method="POST"><h6 class="text-right">*required</h6><label for="img" class="form-label">Select image:</label><input type="file" class="mb-3 modalImg" id="img" name="img" accept="image/*"><div class="titleWrapper"><label for="titleInput" class="form-label titleLabel">*Title</label><input type="text" class="form-control mb-3 modalTitle" id="titleInput"></div><div class="playerWrapper"><label for="playersInput" class="form-label">*Max Amount of Players</label><input type="number" class="form-control mb-3 modalPlayers" id="playersInput"></div><div class="timeWrapper"><label for="timeInput" class="form-label">*Time Needed(in minutes)</label><input type="number" class="form-control mb-3 modalTime" placeholder="(ex: 120)" id="timeInput"></div><label for="descInput" class="form-label">Description</label><textarea class="form-control mb-3 modalDesc" id="descInput"></textarea><button type="button" class="btn submitBtn btn-primary mb-3 mr-3">Submit</button><button type="button" class="btn cancelBtn btn-primary mb-3">Cancel</button></form></div>';
+
+        //display modal
+        main.prepend(modal);
+
+        //get buttons
+        const submit = document.querySelector('.submitBtn'),
+        cancel = document.querySelector('.cancelBtn');
+
+        //button options
+        cancel.addEventListener('click', e => {
+            e.preventDefault();
+            modal.remove();
+        })
+
+        submit.addEventListener('click', e => this.AddGame(e))
+    }
+
+    AddGame(e){
+        e.preventDefault();
+
+        let image = document.querySelector('.modalImg').value,
+        title = document.querySelector('.modalTitle').value,
+        players = document.querySelector('.modalPlayers').value,
+        time = document.querySelector('.modalTime').value,
+        desc = document.querySelector('.modalDesc').value,
+        titleWrap = document.querySelector('.titleWrapper'),
+        playerWrap = document.querySelector('.playerWrapper'),
+        timeWrap = document.querySelector('.timeWrapper');
+
+        if(title === '' || players === '' || time === ''){
+            this.ValidateInputs(title, players, time, titleWrap, playerWrap, timeWrap)
+        }
+    }
+
+    ValidateInputs(title, players, time, titleWrapper, playerWrapper, timeWrapper){
+        //create labels and add attributes as needed
+        let titleError = document.createElement('label'), 
+        playerError = document.createElement('label'),
+        timeError = document.createElement('label'), 
+        inputs = document.querySelectorAll('input');
+
+        titleError.setAttribute('for', 'titleInput');
+        titleError.setAttribute('role', 'alert');
+        titleError.className = 'textError';
+
+        playerError.setAttribute('for', 'playersInput');
+        playerError.setAttribute('role', 'alert');
+        playerError.className = 'textError';
+        
+        timeError.setAttribute('for', 'timeInput');
+        timeError.setAttribute('role', 'alert');
+        timeError.className = 'textError';
+
+        //add in error in order to select later.
+        titleWrapper.appendChild(playerError);
+        playerWrapper.appendChild(playerError);
+        timeWrapper.appendChild(timeError);
+        inputs[1].classList.add('error');
+        inputs[2].classList.add('error');
+        inputs[3].classList.add('error');
+
+        //delete to remove any duplicates
+        titleError.remove();
+        playerError.remove();
+        timeError.remove();
+        inputs[1].classList.remove('error');
+        inputs[2].classList.remove('error');
+        inputs[3].classList.remove('error');
+
+        //now check for error!
+        if(title === ''){
+            titleError.innerHTML = 'Please enter in a title';
+            titleWrapper.appendChild(titleError);
+            inputs[1].classList.add('error');
+        }
+
+        if(players === ''){
+            playerError.innerHTML = 'Please enter in a number';
+            playerWrapper.appendChild(playerError);
+            inputs[2].classList.add('error');
+        }
+
+        if(time === ''){
+            timeError.innerHTML = 'Please enter in a number';
+            timeWrapper.appendChild(timeError);
+            inputs[3].classList.add('error');
+        }
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,124 @@
+window.addEventListener("load", function(){
+    console.log("main page loaded");
+    //instantiate singleton
+    let boardDebut = BoadDebut.getInstance();
+});
+
+class BoadDebut{
+    constructor(){
+        let main = new Main();
+    }
+
+    static getInstance(){
+        if(!BoadDebut._instance){
+            BoadDebut._instance = new BoadDebut();
+            return BoadDebut._instance;
+        }
+        else{
+            throw "Singleton already created in main!";
+        }
+    }
+}
+
+class Main{
+    constructor(){
+        let pickGame = document.querySelector('.pickbtn').addEventListener('click', e => this.RandomGameForm(e));
+    }
+    
+    RandomGameForm(e){
+        e.preventDefault();
+        
+        const main = document.querySelector('main'),
+        carousel = document.querySelector('.carousel'),
+        pickGame = document.querySelector('.pickbtn'),
+        carouselIndicators = document.querySelector('.carousel-indicators'),
+        carouselArrows = document.querySelector('.sideArrows');
+
+        //disable button so multiple forms cant be added
+        pickGame.disabled = true;
+
+        //Create Form Form
+        let modal = document.createElement('div');
+        modal.className = 'modalGame';
+        modal.setAttribute('id', 'modalWindow');
+        modal.innerHTML = '<div class="modal-content"><form class="container col-sm-10 col-md-8 col-lg-6 generateForm" id="newgame" method="POST"><div class="playerWrapper"><label for="playersInput" class="form-label">Number of Players</label><input type="number" class="form-control mb-3 modalPlayers" id="playersInput"></div><div class="timeWrapper"><label for="timeInput" class="form-label">Time Needed(in minutes)</label><input type="number" class="form-control mb-3 modalTime" placeholder="(ex: 120 = 120 minutes)" id="timeInput"></div><button type="button" class="btn submitBtn btn-primary mb-3 mr-3">Submit</button><button type="button" class="btn cancelBtn btn-primary mb-3">Cancel</button></form></div>';
+
+        //Display Form
+        main.prepend(modal);
+
+        //hide other position absolutes
+        carouselIndicators.classList.add('hide');
+        carouselArrows.classList.add('hide');
+
+        //Get Buttons
+        const submit = document.querySelector('.submitBtn'),
+        cancel = document.querySelector('.cancelBtn')
+
+        //Button Options
+        submit.addEventListener('click', e => this.SubmitGame(e));
+
+        cancel.addEventListener('click', e => {
+            e.preventDefault();
+            modal.remove();
+            carouselIndicators.classList.remove('hide');
+            carouselArrows.classList.remove('hide');
+            pickGame.disabled = false;
+        })
+    }
+
+    SubmitGame(e){
+        e.preventDefault();
+
+        let players = document.querySelector('.modalPlayers').value,
+        time = document.querySelector('.modalTime').value,
+        playerWrap = document.querySelector('.playerWrapper'),
+        timeWrap = document.querySelector('.timeWrapper');
+
+        if(players === '' || time === ''){
+            this.ValidateInputs(players, time, playerWrap, timeWrap)
+        }
+        else{
+            //ADD GAME
+        }
+    }
+
+    ValidateInputs(players, time, playerWrapper, timeWrapper){
+        //create labels and add attributes as needed
+        let playerError = document.createElement('label'),
+        timeError = document.createElement('label'),
+        inputs = document.querySelectorAll('input');
+
+        playerError.setAttribute('for', 'playersInput');
+        playerError.setAttribute('role', 'alert');
+        playerError.className = 'textError';
+        
+        timeError.setAttribute('for', 'timeInput');
+        timeError.setAttribute('role', 'alert');
+        timeError.className = 'textError';
+
+        //add in error in order to select later.
+        playerWrapper.appendChild(playerError);
+        timeWrapper.appendChild(timeError);
+        inputs[1].classList.add('error');
+        inputs[0].classList.add('error');
+
+        //delete to remove any duplicates
+        playerError.remove();
+        timeError.remove();
+        inputs[1].classList.remove('error');
+        inputs[0].classList.remove('error');
+
+        //now check for errors
+        if(players === ''){
+            playerError.innerHTML = 'Please enter in a number';
+            playerWrapper.appendChild(playerError);
+            inputs[0].classList.add('error');
+        }
+
+        if(time === ''){
+            timeError.innerHTML = 'Please enter in a number';
+            timeWrapper.appendChild(timeError);
+            inputs[1].classList.add('error');
+        }
+    }
+}
